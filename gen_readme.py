41aef8b467c05abc8702861ea69804b202e85ece
MartinLwx <MartinLwx@163.com>
2023-12-28 14:33:41 +0800

    📝 Only Show the Unfinished Exercises

--- gen_readme.py
+++ gen_readme.py
@@ -4,7 +4,7 @@ from pathlib import Path
 def gen_motivation() -> list[str]:
     return [
         """## Motivation
-After taking CS61 course, I start to apperciate the beauty of Scheme, and I became eager to explore its capabilities further. So I begin to read the famous SCIP book and do exercise.
+After taking CS61 course, I started to appreciate the beauty of Scheme, and I became eager to explore its capabilities further. So I began to read the famous SCIP book and do exercises.
 
 I use [DrRacket](https://docs.racket-lang.org/drracket/index.html) as the IDE to test my code. The explanation will be included in the comment if available."""
     ]
@@ -22,7 +22,7 @@ def get_chapter_title(idx: int) -> str:
 
 def traverse_folder(f: Path, chapter_idx: int) -> list[str]:
     checkboxs = []
-    files = sorted(f.iterdir(), key=lambda x: int(x.stem))
+    files = sorted(f.iterdir(), key=lambda x: int(x.stem if x.stem.isdigit() else x.stem[:-1]))
     assert len(files) > 0, f"Expect at least one file in {f}"
     if files[-1].stem.isdigit():
         max_index = int(files[-1].stem)
@@ -33,19 +33,20 @@ def traverse_folder(f: Path, chapter_idx: int) -> list[str]:
     for i in range(1, max_index + 1):
         i_rkt = f / f"{i:02}.rkt"
         if i_rkt.exists():
-            checkboxs.append(
-                f"- [x] [Exercise {chapter_idx}.{i}](./{f.stem}/{i:02}.rkt)"
-            )
+            continue
+            # checkboxs.append(
+            #     f"- [x] [Exercise {chapter_idx}.{i}](./{f.stem}/{i:02}.rkt)"
+            # )
         else:
             checkboxs.append(
-                f"- [ ] [Exercise {chapter_idx}.{i}](./{f.stem}/{i:02}.rkt)"
+                f"- [ ] Exercise {chapter_idx}.{i}"
             )
 
     return checkboxs
 
 
 def gen_exercises():
-    rows = ["## Exercises"]
+    rows = ["## Unfinished Exercises"]
     for folder in sorted(Path().cwd().iterdir()):
         if folder.is_dir() and folder.stem.startswith("Chapter"):
             print(f"Processing {folder}")
